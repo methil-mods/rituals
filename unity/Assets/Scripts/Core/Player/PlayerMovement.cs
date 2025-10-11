e6ffe81a207d4c345fd2f1c0a7c515cc5b417b0f
Ethan Carollo <etcarollo@gmail.com>
2025-10-11 07:00:35 +0200

    Fix : player movement tile position

--- unity/Assets/Scripts/Core/Player/PlayerMovement.cs
+++ unity/Assets/Scripts/Core/Player/PlayerMovement.cs
@@ -7,17 +7,33 @@ namespace Player
     public class PlayerMovement : MonoBehaviour
     {
         public Tilemap tilemap;
+
+        private Vector3 lastClickTilePos;
+        private Vector3 lastWorldClickPos;
+
         private void Update()
         {
             if (!Mouse.current.leftButton.isPressed) return;
-
             Vector2 mousePos = Mouse.current.position.ReadValue();
             Vector2 worldClickPos = Camera.main.ScreenToWorldPoint(mousePos);
-            Vector3 tilePos = tilemap.WorldToCell(worldClickPos);
-            Debug.Log(tilePos.ToString());
+            
+            Vector3Int cellPos = tilemap.layoutGrid.WorldToCell(worldClickPos);
+            Vector3 tilePos = tilemap.layoutGrid.GetCellCenterWorld(cellPos);
             tilePos.z = 0;
+
             transform.position = tilePos;
+
+            lastClickTilePos = tilePos;
+            lastWorldClickPos = worldClickPos;
         }
 
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(lastClickTilePos, 0.15f);
+
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(lastWorldClickPos, 0.15f);
+        }
     }
 }
