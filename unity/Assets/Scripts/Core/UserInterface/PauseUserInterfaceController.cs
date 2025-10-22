d5a14b50c0946c5aa123cbec572a91d94147ae01
Nak0x <nak0x@proton.me>
2025-10-22 19:03:25 +0200

    Fix: Logarithmic volume changing

--- unity/Assets/Scripts/Core/UserInterface/PauseUserInterfaceController.cs
+++ unity/Assets/Scripts/Core/UserInterface/PauseUserInterfaceController.cs
@@ -31,28 +31,39 @@ namespace Core.UserInterface
             SFXController.Instance.UpdateVolume();
             // Might call an event maybe
         }
+        
+        // Convert slider value [0,1] to perceived-linear volume [0,1]
+        private float SliderToVolume(float sliderValue)
+        {
+            // Avoid log(0)
+            if (sliderValue <= 0f)
+                return 0f;
+
+            // Use a logarithmic scale: typical audio volume curve
+            return Mathf.Pow(sliderValue, 2.0f); // simple power curve
+        }
 
         public void OnMusicChange()
         {
-            SFXDatabase.Instance.musicVolume = musicVolumeSlider.value;
+            SFXDatabase.Instance.musicVolume = SliderToVolume(musicVolumeSlider.value);
             UpdateVolume();
         }
 
         public void OnWalkChange()
         {
-            SFXDatabase.Instance.walkVolume = walkVolumeSlider.value;
+            SFXDatabase.Instance.walkVolume = SliderToVolume(walkVolumeSlider.value);
             UpdateVolume();
         }
 
         public void OnUIChange()
         {
-            SFXDatabase.Instance.pageOpenVolume = uiVolumeSlider.value;
+            SFXDatabase.Instance.pageOpenVolume = SliderToVolume(uiVolumeSlider.value);
             UpdateVolume();
         }
 
         public void OnOtherChange()
         {
-            SFXDatabase.Instance.otherVolume = otherVolumeSlider.value;
+            SFXDatabase.Instance.otherVolume = SliderToVolume(otherVolumeSlider.value);
             UpdateVolume();
         }
     }
